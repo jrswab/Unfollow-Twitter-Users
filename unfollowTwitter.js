786b32d5ab3a84191b7f3b4c0e1cb7cc82f7ea7d
jrswab <jrswab@gmail.com>
2017-08-27 22:40:32 -0400

    Updated JavaScript to allow for skipping users, execute unfollowing at a random interval, and show a message after completion

--- unfollowTwitter.js
+++ unfollowTwitter.js
@@ -1,15 +1,43 @@
+const userField = document.getElementsByClassName("ProfileCard-userFields");
+const input = window.prompt("Enter any usernames you want to continue following separated by a space.");
 let i = 0;
-let unfollowers = document.getElementsByClassName("ProfileCard-userFields");
+let t = 0;
+let inputFound = [];
+let saveNames = input.split(' ');
+let rand = Math.round(Math.random() * 10000);
+
+findUsers();
+
+function findUsers(){
+	if(t < userField.length){
+		for(u = 0; u < saveNames.length; u++){
+			if(userField[t].children[1].innerText.includes(saveNames[u])){
+				inputFound[t] = true;
+				break;
+			}else{
+				inputFound[t] = false;
+			}
+		}
+		t++;
+		if(t < userField.length){
+			findUsers();
+		}
+	}
+}
+
 unfollowLoop();
 
 function unfollowLoop(){
     setTimeout(function(){
-        if(unfollowers[i].children[1].children[2] === undefined){
-	            unfollowers[i].previousElementSibling.children[0].children[0].children[0].children[1].click();
-        }
+        if(userField[i].children[1].innerText.includes("Follows you") === false && inputFound[i] === false){
+		userField[i].previousElementSibling.children[0].children[0].children[0].children[1].click();
+	}
         i++;
-        if(i < unfollowers.length){
-            unfollowLoop();
-        }
-    }, 2000)
+        if(i < userField.length){
+		rand = Math.round(Math.random() * 10000);
+		unfollowLoop();
+        }else{
+		window.alert("Unfollow Script Finished!");
+	}
+    }, rand)
 }
