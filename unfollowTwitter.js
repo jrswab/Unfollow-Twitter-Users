bd62f51bc25d22db05a104fa1dff1344ce30f08e
jrswab <jrswab@gmail.com>
2017-08-29 22:21:31 -0400

    Added option to set delay by user

--- unfollowTwitter.js
+++ unfollowTwitter.js
@@ -1,13 +1,28 @@
 const userField = document.getElementsByClassName("ProfileCard-userFields");
 const input = window.prompt("Enter any usernames you want to continue following separated by a space.");
+let inputMax = window.prompt("Enter maximum number of seconds to wait between actions. Must be greater than '2'.");
 let i = 0;
 let t = 0;
 let inputFound = [];
-let saveNames = input.split(' ');
+let saveNames = input.split(" ");
 let button = undefined;
 let rand = Math.round(Math.random() * 10000);
+let setMin = 2000;
+let setMax = inputMax * 1000;
+main();
 
-findUsers();
+function main(){
+    if(inputMax > 2){
+        randomIntFromInterval(setMin,setMax);
+        unfollowLoop();
+        findUsers();
+    }else if(max <= 2000){
+    	inputMax = window.prompt("Please enter a number great than 2");
+    	setMax = inputMax * 1000;
+        main();
+    }
+}
+
 
 function findUsers(){
 	if(t < userField.length){
@@ -26,20 +41,27 @@ function findUsers(){
 	}
 }
 
-unfollowLoop();
+function randomIntFromInterval(min,max)
+{
+	rand = Math.floor(Math.random()*(max-min+1)+min);
+	return rand;
+}
+
 
 function unfollowLoop(){
 	button = userField[i].previousElementSibling.children[0].children[0].children[0].children[1];
-	buttonText = button.innerText;
-    setTimeout(function(){
-        if(userField[i].children[1].innerText.includes("Follows you") === false && inputFound[i] === false && buttonText === "Following"){
-		button.click();
-                i++;
-                if(i < userField.length){
-		        rand = Math.round(Math.random() * 10000);
-		        unfollowLoop();
-                }else{
-		        window.alert("Unfollow Script Finished!");
-	}
-    }, rand)
+	let buttonText = button.innerText;
+	setTimeout(function(){
+		if(userField[i].children[1].innerText.includes("Follows you") === false && inputFound[i] === false && buttonText === "Following"){
+			button.click();
+        }
+		i++;
+		if(i < userField.length){
+				randomIntFromInterval(setMin,setMax);
+				unfollowLoop();
+		}else{
+			window.alert("Unfollow Script Finished!");
+		}
+	}, rand)
 }
+
